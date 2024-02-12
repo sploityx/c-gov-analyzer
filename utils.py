b4666b9783eb3c816f972839fceac5e4a4640a22
sploityx <florian.aschenbrenner@posteo.de>
2024-02-12 11:39:11 +0100

    Added read_msr to utils

--- utils.py
+++ utils.py
@@ -11,3 +11,10 @@ def exec_cmd(cmd: str):
 def exec_shell_cmd(cmd: str):
     print(f"{os.path.basename(__file__)} will execute \"{cmd}\" now")
     subprocess.run(cmd, shell=True, check=True)
+
+# need sudo rights to access msr device
+def read_msr(msr, cpu=0):
+    msr_f = os.open(f'/dev/cpu/{cpu}/msr', os.O_RDONLY)
+    os.lseek(msr_f, msr, os.SEEK_SET)
+    msr_val = os.read(msr_f, 8)
+    return int.from_bytes(msr_val, byteorder='little')
