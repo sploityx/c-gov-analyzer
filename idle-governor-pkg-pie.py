5f360163f4eee9b152235b969a0c224191a4a741
sploityx <florian.aschenbrenner@posteo.de>
2024-02-16 23:22:19 +0100

    add skeleton for spearman correlation

--- idle-governor-pkg-pie.py
+++ idle-governor-pkg-pie.py
@@ -1,16 +1,59 @@
 #!/usr/bin/python3
 
-import json
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.stats import spearmanr
 
-with open('stat-data.json', 'r', encoding='utf-8') as file:
-    df = pd.read_json(file, lines=True)
 
-df = df[df['event'].str.startswith('cstate_pkg/')]
-df['event'] = df['event'].str.replace('cstate_pkg/', '').str[:-1]
-# remove trailing zeroes
-df['counter-value'] = pd.to_numeric(df['counter-value'], downcast='float')
+def read_data():
+    '''Reads the output of the perf-stat command and returns a pandas df'''
+    with open('stat-data.json', 'r', encoding='utf-8') as file:
+        df = pd.read_json(file, lines=True)
+    return df
 
-plt.pie(df['counter-value'], labels=df['event'], autopct='%1.1f%%', startangle=90)
-plt.show()
+
+def filter_cstage_pkg(df):
+    '''Filters and formats events for cstage_pkg'''
+    df = df[df['event'].str.startswith('cstate_pkg/')]
+    df['event'] = df['event'].str.replace('cstate_pkg/', '').str[:-1]
+    return df
+
+
+def filter_format(df, event : str):
+    '''Filters and formats the dataframe for event'''
+    df = df[df['event'].str.startswith(event)]
+    #df['event'] = df['event'].str.replace(event, '').str[:-1]
+    return df
+
+
+def pkg_pie_chart(df):
+    '''Creates a pie chart for used C-States on pkg_level'''
+    df_c_pkg = filter_format(df, 'cstate_pkg/')
+    plt.pie(df_c_pkg['counter-value'], labels=df_c_pkg['event'], autopct='%1.1f%%', startangle=90)
+    plt.show()
+
+
+def correlation(df):
+    '''Calculates the spearman rank-order correlation between pkg c-states and energy'''
+    df_c_pkg = filter_format(df, 'cstate_pkg')
+    df_power_pkg = filter_format(df, 'power/energy-pkg/')
+    for _,c_pkg in df_c_pkg.iterrows():
+        # we need lots more data for this to work as intented
+        corr_coef, p_value = spearmanr(c_pkg['counter-value'], float(df_power_pkg['counter-value']))
+        if p_value > 0.5:
+            plt.scatter(corr_coef, c_pkg['counter-value'], c='green', label=c_pkg['event'])
+        else:
+            plt.scatter(corr_coef, c_pkg['counter-value'], c='red', label=c_pkg['event'])
+    plt.show()
+
+
+
+def main():
+    '''Visualizes the output of the perf-stat json from the collect wrapper'''
+    df = read_data()
+    pkg_pie_chart(df)
+    correlation(df)
+
+
+if __name__ == '__main__':
+    main()
