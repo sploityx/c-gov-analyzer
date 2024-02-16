0b3bb51b2189e4af5eef7ea3aac555966d760f5c
sploityx <florian.aschenbrenner@posteo.de>
2024-02-16 16:20:00 +0100

    pie chart supports new json

--- idle-governor-pkg-pie.py
+++ idle-governor-pkg-pie.py
@@ -4,17 +4,13 @@ import json
 import pandas as pd
 import matplotlib.pyplot as plt
 
-with open('pkg-c-state.json', 'r', encoding='utf-8') as file:
-    data = [json.loads(line) for line in file]
+with open('stat-data.json', 'r', encoding='utf-8') as file:
+    df = pd.read_json(file, lines=True)
 
-counter_values = [entry['counter-value'] for entry in data]
-event_names = [entry['event'].split('/')[1] for entry in data]
-
-df = pd.DataFrame({'counter-value': counter_values, 'event': event_names})
-
-# remove trialing zeroes
+df = df[df['event'].str.startswith('cstate_pkg/')]
+df['event'] = df['event'].str.replace('cstate_pkg/', '').str[:-1]
+# remove trailing zeroes
 df['counter-value'] = pd.to_numeric(df['counter-value'], downcast='float')
 
-# Plot a pie chart
 plt.pie(df['counter-value'], labels=df['event'], autopct='%1.1f%%', startangle=90)
 plt.show()
